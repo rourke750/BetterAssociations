3f97cd4c6aad1b59665acc6b5999e24e1ea3a9f2
Rourke750 <Rourke7501@gmail.com>
2014-06-19 14:57:54 -0400

    d

--- java/com/sandislandserv/rourke750/Listener/LoginManager.java
+++ java/com/sandislandserv/rourke750/Listener/LoginManager.java
@@ -5,100 +5,111 @@ import java.lang.reflect.Modifier;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
-import net.minecraft.server.v1_7_R1.EntityHuman;
+import net.minecraft.server.v1_7_R3.EntityHuman;
 import net.minecraft.util.com.mojang.authlib.GameProfile;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.craftbukkit.v1_7_R1.entity.CraftHumanEntity;
+import org.bukkit.craftbukkit.v1_7_R3.entity.CraftHumanEntity;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 
 import com.sandislandserv.rourke750.BetterAssociations;
+import com.sandislandserv.rourke750.Information.SendInformation;
 import com.sandislandserv.rourke750.database.BaseValues;
 
-public class LoginManager implements Listener{
-	
+public class LoginManager implements Listener {
+
 	private BaseValues db;
 	private BetterAssociations plugin;
-	
-	public LoginManager(BaseValues db, BetterAssociations plugin){
+
+	public LoginManager(BaseValues db, BetterAssociations plugin, SendInformation si) {
 		this.db = db;
 		this.plugin = plugin;
 	}
-	
+
 	@EventHandler(priority = EventPriority.LOWEST)
-	public void joinEvent(PlayerJoinEvent event){
+	public void joinEvent(PlayerJoinEvent event) {
 		final Player player = event.getPlayer();
-		Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable(){
+		Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
 			@Override
-			public void run(){
+			public void run() {
 				db.addPlayerIp(player);
 				db.associatePlayer(player);
 			}
 		});
 	}
-	
+
 	@EventHandler(priority = EventPriority.LOWEST)
-	public void loginEvent(PlayerLoginEvent event){ // use AsyncPlayerPreLoginEvent with 1.7.5
-		Player player = event.getPlayer();
-		// set the player display name.  Plugins by now should be running based by uuid
-			// This just helps show every person as who they were when they first logged on
-		db.addPlayerUUID(player);
-			String name = db.getPlayer(player.getUniqueId().toString());
-			if (name == null) return;
-			try {
-				
-				// start of getting the GameProfile
-				CraftHumanEntity craftHuman = (CraftHumanEntity) player;
-				EntityHuman human = craftHuman.getHandle();
-				Field fieldName = EntityHuman.class.getDeclaredField("i");
-				fieldName.setAccessible(true);
-				GameProfile prof = (GameProfile) fieldName.get(human);
-				// End
-				
-				//Start of adding a new name
-				Field nameUpdate = prof.getClass().getDeclaredField("name");
-				
-				setFinalStatic(nameUpdate, name, prof);
-				// end
-			} catch (NoSuchFieldException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SecurityException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalArgumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			player.setDisplayName(name);
-			player.setPlayerListName(name);
-			player.setCustomName(name);
+	public void preLoginEvent(AsyncPlayerPreLoginEvent event) {
+		String name = event.getName();
+		String uuid = event.getUniqueId().toString();
+		db.addPlayerUUID(name, uuid);
 	}
-	
-	static void setFinalStatic(Field field, Object newValue, GameProfile prof){
+
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void loginEvent(PlayerLoginEvent event) {
+		// set the player display name. Plugins by now should be running based
+		// by uuid
+		// This just helps show every person as who they were when they first
+		// logged on
+		Player player = event.getPlayer();
+		String name = db.getPlayer(player.getUniqueId().toString());
 		try {
-		    field.setAccessible(true);
+			// start of getting the GameProfile
+			CraftHumanEntity craftHuman = (CraftHumanEntity) player;
+			EntityHuman human = craftHuman.getHandle();
+			Field fieldName = EntityHuman.class.getDeclaredField("i");
+			fieldName.setAccessible(true);
+			GameProfile prof = (GameProfile) fieldName.get(human);
+			// End
 
-		    // remove final modifier from field
-		    Field modifiersField;
+			// Start of adding a new name
+			Field nameUpdate = prof.getClass().getDeclaredField("name");
+
+			setFinalStatic(nameUpdate, name, prof);
+			// end
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		player.setDisplayName(name);
+		player.setPlayerListName(name);
+		player.setCustomName(name);
+	}
+
+	static void setFinalStatic(Field field, Object newValue, GameProfile prof) {
+		try {
+			field.setAccessible(true);
+
+			// remove final modifier from field
+			Field modifiersField;
 			modifiersField = Field.class.getDeclaredField("modifiers");
-		    modifiersField.setAccessible(true);
-		    modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+			modifiersField.setAccessible(true);
+			modifiersField
+					.setInt(field, field.getModifiers() & ~Modifier.FINAL);
 
-		    field.set(prof, newValue);
+			field.set(prof, newValue);
 		} catch (NoSuchFieldException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -113,18 +124,20 @@ public class LoginManager implements Listener{
 			e.printStackTrace();
 		}
 	}
-	
+
 	@EventHandler(priority = EventPriority.MONITOR)
-	public void sendAdminMessages(PlayerJoinEvent event){
+	public void sendAdminMessages(PlayerJoinEvent event) {
 		Player playerLogger = event.getPlayer();
 		int amount = db.getAmountOfAlts(playerLogger);
-		if (amount == 0) return;
+		if (amount == 0)
+			return;
 		Set<OfflinePlayer> admins = Bukkit.getOperators();
-		for (OfflinePlayer admin: admins){
-			if (!admin.isOnline()) continue;
+		for (OfflinePlayer admin : admins) {
+			if (!admin.isOnline())
+				continue;
 			Player player = admin.getPlayer();
-			player.sendMessage(ChatColor.RED + playerLogger.getDisplayName() + " logged in and has " + 
-			amount +" alts.");
+			player.sendMessage(ChatColor.RED + playerLogger.getDisplayName()
+					+ " logged in and has " + amount + " alts.");
 		}
 	}
 
